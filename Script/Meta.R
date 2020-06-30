c82defe62a0cf787b544e1f289ec5a990a2b6ab4
Eduardo de Freitas Costa <60922090+eduardodefreitascosta@users.noreply.github.com>
2020-06-30 22:42:11 +0200

    Nova analise

--- Script/Meta.R
+++ Script/Meta.R
@@ -5,6 +5,7 @@ library(bayesmeta)
 library(ggplot2)
 library(here)
 library(readxl)
+library(tidyverse)
 ##creating function##
 
 
@@ -58,7 +59,33 @@ meta1[[i]]<-bayesmeta(y=crins[[i]][,"yi"],sigma=sqrt(crins[[i]][,"vi"]),mu.prior
 
 }
 
-plot(1, type="n", xlab="Prevalence", ylab="Density", xlim=c(0, 1), ylim=c(0, 15),main="Posterior prevalence density")
+
+
+
+data<-data.frame(matrix(rep(NA,10000*6), nrow=10000,ncol=6,byrow=T))
+
+for (j in 1: length(N)){
+  for(i in 1:10000){
+ data[i,j]<-rnorm(1,meta1[[j]]$summary[3,2],meta1[[j]]$summary[4,2]) 
+
+  }
+}
+ data$id<-seq(1:10000)
+   
+ colnames(data)<-id
+ 
+ data2 <- gather(data = data,
+                        key = "Pais",
+                        value = "Prev",-id)
+ 
+
+ ggplot(data2, aes(x = Pais, y = back(Prev)*100)) +
+   geom_boxplot()+
+   scale_y_continuous(name = "Prevalence %",breaks = seq(0, 100, 15),
+                      limits=c(0, 85))+
+   scale_x_discrete(name = "Region")
+ 
+ 
 saida<-array(rep(NA,30),dim=c(6,5,1))
 
 for (i in 1:length(N)){
@@ -78,7 +105,7 @@ saida[i, ,1]<-cbind( (meta1[[i]]$summary[3,2]),
 
 
 ##Cut vs carcass analysis##
-
+id1<-c("Carcass","Cut")
 meta2<-list()
 crins1<-list()
 corte<-c(1,2)
@@ -91,16 +118,35 @@ for (i in 1:length(corte)){
 }
 
 
-plot(1, type="n", xlab="Prevalence", ylab="Density", xlim=c(0, 0.4), ylim=c(0, 16),main="Posterior prevalence density")
+
+data3<-data.frame(matrix(rep(NA,10000*2), nrow=10000,ncol=2,byrow=T))
+
+for (j in 1:length(corte)){
+  for(i in 1:10000){
+    data3[i,j]<-rnorm(1,meta2[[j]]$summary[3,2],meta2[[j]]$summary[4,2]) 
+    
+  }
+}
+
+
+colnames(data3)<-id1
+data3$id<-seq(1:10000)
+
+data4 <- gather(data = data3,
+                key = "Cut",
+                value = "Prev",-id)
+
+
+ggplot(data4, aes(x = Cut, y = back(Prev)*100)) +
+  geom_boxplot()+
+  scale_y_continuous(name = "Prevalence %",breaks = seq(0, 100, 10),
+                     limits=c(5, 40))+
+  scale_x_discrete(name = "")
+
+
 saida1<-array(rep(NA,10),dim=c(2,5,1))
-id1<-c("Carcass","Cut")
 
 for (i in 1:length(corte)){
-  
-  lines(density(back(rnorm(10000,meta2[[i]]$summary[3,2],meta2[[i]]$summary[4,2]))),col=corte[i])
-  
-  
-  legend(0.3, 6, bty='n', id1, col=c(1:i),lty=1)
   
   
   saida1[i, ,1]<-cbind((meta2[[i]]$summary[3,2]),
@@ -128,19 +174,40 @@ for (i in 1:length(loc)){
 }
 
 
-plot(1, type="n", xlab="Prevalence", ylab="Density", xlim=c(0, 0.4), ylim=c(0, 17),main="Posterior prevalence density")
+
+data5<-data.frame(matrix(rep(NA,10000*2), nrow=10000,ncol=2,byrow=T))
+id2<-c("Retail","Slaugterhouse")
+for (j in 1:length(loc)){
+  for(i in 1:10000){
+    data5[i,j]<-rnorm(1,meta3[[j]]$summary[3,2],meta3[[j]]$summary[4,2]) 
+    
+  }
+}
+
+
+colnames(data5)<-id2
+data5$id<-seq(1:10000)
+
+data6 <- gather(data = data5,
+                key = "local",
+                value = "Prev",-id)
+
+
+ggplot(data6, aes(x = local, y = back(Prev)*100)) +
+  geom_boxplot()+
+  scale_y_continuous(name = "Prevalence %",breaks = seq(0, 100, 10),
+                     limits=c(5, 40))+
+  scale_x_discrete(name = "")
+
+
+
+
 
 saida2<-array(rep(NA,10),dim=c(2,5,1))
 
-id2<-c("Retail","Slaugterhouse")
+
 for (i in 1:length(loc)){
-  
-  
-  lines(density(back(rnorm(10000,meta3[[i]]$summary[3,2],meta3[[i]]$summary[4,2]))),col=loc[i])
-  
-  
-  legend(0.3, 9, bty='n', id2, col=c(1:i),lty=1)
-  
+ 
   
   saida2[i, ,1]<-cbind((meta3[[i]]$summary[3,2]),
                        (meta3[[i]]$summary[4,2]),
@@ -148,6 +215,75 @@ for (i in 1:length(loc)){
                        (meta3[[i]]$summary[2,2]),
                        (meta3[[i]]$summary[6,2]) )
 }
+
+
+
+
+##fresh vs frozen##
+
+
+meta4<-list()
+crins3<-list()
+fro<-c(1,2)
+
+claudia1<-na.omit(claudia)
+
+for (i in 1:length(fro)){
+  
+  crins3[[i]]<-escalc(measure = "PLO",xi=pos,ni=total,data=claudia1,subset = fresh==loc[i])
+  
+  meta4[[i]]<-bayesmeta(y=na.omit(crins3[[i]][,"yi"]),sigma=sqrt(na.omit(crins3[[i]][,"vi"])),mu.prior.mean = 1,mu.prior.sd =4,tau.prior=function(t){dhalfnormal(t,scale=0.5)} )
+  
+}
+
+
+data7<-data.frame(matrix(rep(NA,10000*2), nrow=10000,ncol=2,byrow=T))
+id3<-c("Fresh","Frozen")
+for (j in 1:length(fro)){
+  for(i in 1:10000){
+    data7[i,j]<-rnorm(1,meta4[[j]]$summary[3,2],meta4[[j]]$summary[4,2]) 
+    
+  }
+}
+
+
+colnames(data7)<-id3
+data7$id<-seq(1:10000)
+
+data8 <- gather(data = data7,
+                key = "fro",
+                value = "Prev",-id)
+
+
+ggplot(data8, aes(x = fro, y = back(Prev)*100)) +
+  geom_boxplot()+
+  scale_y_continuous(name = "Prevalence %",breaks = seq(0, 100, 10),
+                     limits=c(5, 40))+
+  scale_x_discrete(name = "")
+
+
+
+
+
+saida3<-array(rep(NA,10),dim=c(2,5,1))
+
+
+for (i in 1:length(loc)){
+  
+  
+  lines(density(back(rnorm(10000,meta4[[i]]$summary[3,2],meta4[[i]]$summary[4,2]))),col=loc[i])
+  
+  
+  legend(0.3, 9, bty='n', id2, col=c(1:i),lty=1)
+  
+  
+  saida3[i, ,1]<-cbind((meta4[[i]]$summary[3,2]),
+                       (meta4[[i]]$summary[4,2]),
+                       (meta4[[i]]$summary[5,2]),
+                       (meta4[[i]]$summary[2,2]),
+                       (meta4[[i]]$summary[6,2]) )
+}
+
 
 
 
